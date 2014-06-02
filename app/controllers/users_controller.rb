e1c3b4aa1b597f5caeed2c30ce0114f26b603356
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-06-02 13:24:16 +0200

    Stub User show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
   def new
     @title = "Sign up"
+  end
+  
+  def show
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
+  resources :users
   root :to => "pages#home"
 
-  match "/contact", to: "pages#contact", via: :get
-  match "/about", to: "pages#about", via: :get
-  match "/help", to: "pages#help", via: :get
-  match "/signup", to: "users#new", via: [:get, :post]
+  match "/contact", :to => "pages#contact", :via => :get
+  match "/about",    :to =>  "pages#about",   :via => :get
+  match "/help",       :to => "pages#help",      :via => :get
+  match "/signup",   :to => "users#new",       :via => [:get, :post]
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
